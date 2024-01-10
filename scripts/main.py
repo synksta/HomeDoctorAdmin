b2f633846ba91d660959d1865d3418bb567359cd
synksta <contriturbamentum@gmail.com>
2024-01-10 20:45:36 +0700

    little addition do main.py, nothing special just learning kivy

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+
 class MyGridLayout(GridLayout):
   # Initialize infinite keywords
   def __init__(self, **kwargs):
@@ -39,7 +40,24 @@ class MyGridLayout(GridLayout):
 
     # Create a submit button
     self.submit = Button(text="Submit", font_size= 32)
+    # Bind the button 
+    self.submit.bind(on_press=self.press)
     self.add_widget(self.submit)
+
+  def press(self, instance):
+    name =  self.name.text
+    midname = self.midname.text
+    surname = self.surname.text
+
+    # print(f'Hello {name} {midname} {surname}!')
+    # Create a result label
+    self.add_widget(Label(f'Hello {name} {midname} {surname}!'))
+
+    # Clear the input boxes
+
+    self.name.text = ''
+    self.midname.text = ''
+    self.surname.text = ''
 
 class MyApp(App):
   def build(self):
